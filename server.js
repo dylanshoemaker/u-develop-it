35397a0546761eaea5c9ec9d188bd2a1e5bb1e09
Dylan Shoemaker <me@dylanshoemaker.com>
2021-09-03 22:19:48 -0700

    set up .env for passwords, connect to mysql db

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
+require('dotenv').config();
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
+const PORT = process.env.PORT
 const app = express();
+
+
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: process.env.DB_USER,
+    // Your MySQL password
+    password: process.env.DB_PASS,
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
