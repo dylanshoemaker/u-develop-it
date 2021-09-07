cf915c03ae77644e87a9e10fd3e760b7bb56e17f
Dylan Shoemaker <me@dylanshoemaker.com>
2021-09-06 17:16:21 -0700

    create votes table

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,7 +18,7 @@ VALUES
   ('Unica', 'Zurn', NULL, 1);
 
 
-  INSERT INTO voters (first_name, last_name, email)
+INSERT INTO voters (first_name, last_name, email)
 VALUES
   ('James', 'Fraser', 'jf@goldenbough.edu'),
   ('Jack', 'London', 'jlondon@ualaska.edu'),
